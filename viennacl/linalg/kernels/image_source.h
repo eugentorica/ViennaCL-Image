5cae5db12e6b0159077375ea0df8dfa5f2224269
Jenea <jenea@linux-nut5.(none)>
2011-12-19 22:38:45 +0200

    Image kernel functions updated.

--- viennacl/linalg/kernels/image_source.h
+++ viennacl/linalg/kernels/image_source.h
@@ -13,31 +13,31 @@ namespace linalg {
 namespace kernels {
 
 const char * const image_add =
-		" \n"
-				"__kernel void add(\n"
-				"          read_only image2d_t src1,\n"
-				"          read_only image2d_t src2,\n"
-				"		   write_only image2d_t dst)\n"
-				"{ \n"
-        "  int w = min(get_image_width(src1), get_image_width(src2));\n"
-        "  int h = min(get_image_height(src1), get_image_height(src2));\n"
-        "  int coordW = get_global_id(0); \n"
-        "  int coordH = get_global_id(1); \n"
-        "  int2 coord = (int2)(coordW , coordH); // y - coord  \n"
-				"  const sampler_t sampler = CLK_NORMALIZED_COORDS_FALSE | //Natural coordinates \n"
-				"                      CLK_ADDRESS_REPEAT | \n"
-				"                      CLK_FILTER_NEAREST; //Don't interpolate \n"
-				"  float4 src1_val = read_imagef(src1,sampler,coord); \n"
-				"  float4 src2_val = read_imagef(src2,sampler,coord); \n"
-				"  float4 val= src1_val + src2_val; \n"
-        "  if(coordW < w && coordH < h)\n"
-        "     write_imagef(dst,coord,val); \n"
-				"}\n"
-				" \n"; //image_add
+    " \n"
+    "__kernel void add(\n"
+    "          read_only image2d_t src1,\n"
+    "          read_only image2d_t src2,\n"
+    "		   write_only image2d_t dst)\n"
+    "{ \n"
+    "  int w = min(get_image_width(src1), get_image_width(src2));\n"
+    "  int h = min(get_image_height(src1), get_image_height(src2));\n"
+    "  int coordW = get_global_id(0); \n"
+    "  int coordH = get_global_id(1); \n"
+    "  int2 coord = (int2)(coordW , coordH); // y - coord  \n"
+    "  const sampler_t sampler = CLK_NORMALIZED_COORDS_FALSE | //Natural coordinates \n"
+    "                      CLK_ADDRESS_REPEAT | \n"
+    "                      CLK_FILTER_NEAREST; //Don't interpolate \n"
+    "  float4 src1_val = read_imagef(src1,sampler,coord); \n"
+    "  float4 src2_val = read_imagef(src2,sampler,coord); \n"
+    "  float4 val= src1_val + src2_val; \n"
+    "  if(coordW < w && coordH < h)\n"
+    "     write_imagef(dst,coord,val); \n"
+    "}\n"
+    " \n"; //image_add
 
 const char * const image_sub =
     " \n"
-		"__kernel void sub(\n"
+    "__kernel void sub(\n"
     "          read_only image2d_t src1,\n"
     "          read_only image2d_t src2,\n"
     "      write_only image2d_t dst)\n"
@@ -56,7 +56,7 @@ const char * const image_sub =
     "  if(coordW < w && coordH < h)\n"
     "     write_imagef(dst,coord,val); \n"
     "}\n"
-		" \n"; //image_sub
+    " \n"; //image_sub
 
 const char* const filter2D =
     "__kernel void filter2D(read_only image2d_t srcImg,write_only image2d_t dstImg, \n"
